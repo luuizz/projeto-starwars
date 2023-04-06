17542b492df2cd47cf608950df8589be917cfbf9
luuizz <luu.izz@hotmail.com>
2023-04-05 22:08:58 -0300

    Ajustes feitos

--- main.js
+++ main.js
@@ -1,3 +1,63 @@
+// Transições entre páginas
+/*function delay(n) {
+  n = n || 2000;
+  return new Promise((done) => {
+      setTimeout(() => {
+          done();
+      }, n);
+  });
+}
+
+function pageTransition() {
+  var tl = gsap.timeline();
+  tl.to(".loading-screen", {
+      duration: 1.2,
+      width: "100%",
+      left: "0%",
+      ease: "Expo.easeInOut",
+  });
+
+  tl.to(".loading-screen", {
+      duration: 1,
+      width: "100%",
+      left: "100%",
+      ease: "Expo.easeInOut",
+      delay: 0.3,
+  });
+  tl.set(".loading-screen", { left: "-100%" });
+}
+
+function contentAnimation() {
+  var tl = gsap.timeline();
+  tl.from(".animate-this", { duration: 1, y: 30, opacity: 0, stagger: 0.4, delay: 0.2 });
+}
+
+$(function () {
+  barba.init({
+      sync: true,
+
+      transitions: [
+          {
+              async leave(data) {
+                  const done = this.async();
+
+                  pageTransition();
+                  await delay(1000);
+                  done();
+              },
+
+              async enter(data) {
+                  contentAnimation();
+              },
+
+              async once(data) {
+                  contentAnimation();
+              },
+          },
+      ],
+  });
+});*/
+
 // Swiper seção de mapas
 
 var swiper = new Swiper(".mySwiper", {
@@ -18,27 +78,4 @@ var swiper = new Swiper(".mySwiper", {
         slidesPerView: 3.5,
       },
     }
-    });
-
-
-// Swiper seção de seleção de personagens
-
-var swiper = new Swiper(".mySwiper", {
-  spaceBetween: 10,
-  slidesPerView: 4,
-  freeMode: true,
-  grabCursor: false,
-  watchSlidesProgress: true,
-});
-
-
-var swiper2 = new Swiper(".mySwiper2", {
-  spaceBetween: 10,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  thumbs: {
-    swiper: swiper,
-  },
-});
+  });
