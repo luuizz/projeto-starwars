7d0382b7a883bd4375078e338300ec50fef2b1db
luuizz <luu.izz@hotmail.com>
2023-04-21 16:31:48 -0300

    Integrado a API SWAPI na seção de seleção de personagens

--- main.js
+++ main.js
@@ -84,3 +84,171 @@ var swiper = new Swiper(".slide-midia", {
     },
   }
 });
+
+// Integração da API
+
+// var xhr = new XMLHttpRequest();
+
+// var resultado = null;
+
+// xhr.open('GET', 'https://swapi.dev/api/people/1');
+// xhr.send();
+
+// // const naves = document.querySelectorAll('#starships');
+// // const genero = document.querySelectorAll('#gender');
+// // const veiculos = document.querySelectorAll('#vehicles');
+// // const armas = document.querySelectorAll('#weapons');
+
+
+// xhr.onreadystatechange = function() {
+//   if (xhr.readyState == 4 && xhr.status == 200) {
+//     resultado = JSON.parse(xhr.response);
+//     console.log(resultado);
+//   }
+// }
+
+// var genero = resultado[0].gender;
+
+// APi Luke
+// let baseUrl = 'https://swapi.dev/api/people/44/';
+// let genero = document.getElementById('gender');
+// let corCabelo = document.getElementById('skin-color');
+// let anoNascimento = document.getElementById('ano-nascimento');
+// let altura = document.getElementById('altura');
+
+// fetch(baseUrl).then(response => response.json())
+// .then(json =>{
+//   console.log(json)
+//   genero.innerHTML = `${json.gender}`
+//   corCabelo.innerHTML = `${json.hair_color}`
+//   anoNascimento.innerHTML = `${json.birth_year}`
+//   altura.innerHTML = `${json.height}`
+// })
+
+fetch('https://swapi.dev/api/people/1/')
+  .then(response => response.json())
+  .then(data => {
+    const genderElement = document.getElementById('slide-luke').querySelector('#gender');
+    genderElement.textContent = data.gender;
+
+    const cabeloElement = document.getElementById('slide-luke').querySelector('#skin-color');
+    cabeloElement.textContent = data.hair_color;
+
+    const anoElement = document.getElementById('slide-luke').querySelector('#ano-nascimento');
+    anoElement.textContent = data.birth_year;
+
+    const alturaElement = document.getElementById('slide-luke').querySelector('#altura');
+    alturaElement.textContent = data.height;
+  });
+
+  fetch('https://swapi.dev/api/people/5/')
+  .then(response => response.json())
+  .then(data => {
+    const genderElement = document.getElementById('slide-leia').querySelector('#gender');
+    genderElement.textContent = data.gender;
+
+    const cabeloElement = document.getElementById('slide-leia').querySelector('#skin-color');
+    cabeloElement.textContent = data.hair_color;
+
+    const anoElement = document.getElementById('slide-leia').querySelector('#ano-nascimento');
+    anoElement.textContent = data.birth_year;
+
+    const alturaElement = document.getElementById('slide-leia').querySelector('#altura');
+    alturaElement.textContent = data.height;
+  });
+
+  fetch('https://swapi.dev/api/people/14/')
+  .then(response => response.json())
+  .then(data => {
+    const genderElement = document.getElementById('slide-han').querySelector('#gender');
+    genderElement.textContent = data.gender;
+
+    const cabeloElement = document.getElementById('slide-han').querySelector('#skin-color');
+    cabeloElement.textContent = data.hair_color;
+
+    const anoElement = document.getElementById('slide-han').querySelector('#ano-nascimento');
+    anoElement.textContent = data.birth_year;
+
+    const alturaElement = document.getElementById('slide-han').querySelector('#altura');
+    alturaElement.textContent = data.height;
+  });
+
+  fetch('https://swapi.dev/api/people/22/')
+  .then(response => response.json())
+  .then(data => {
+    const genderElement = document.getElementById('slide-boba').querySelector('#gender');
+    genderElement.textContent = data.gender;
+
+    const cabeloElement = document.getElementById('slide-boba').querySelector('#skin-color');
+    cabeloElement.textContent = data.hair_color;
+
+    const anoElement = document.getElementById('slide-boba').querySelector('#ano-nascimento');
+    anoElement.textContent = data.birth_year;
+
+    const alturaElement = document.getElementById('slide-boba').querySelector('#altura');
+    alturaElement.textContent = data.height;
+  });
+
+  fetch('https://swapi.dev/api/people/4/')
+  .then(response => response.json())
+  .then(data => {
+    const genderElement = document.getElementById('slide-vader').querySelector('#gender');
+    genderElement.textContent = data.gender;
+
+    const cabeloElement = document.getElementById('slide-vader').querySelector('#skin-color');
+    cabeloElement.textContent = data.hair_color;
+
+    const anoElement = document.getElementById('slide-vader').querySelector('#ano-nascimento');
+    anoElement.textContent = data.birth_year;
+
+    const alturaElement = document.getElementById('slide-vader').querySelector('#altura');
+    alturaElement.textContent = data.height;
+  });
+
+  fetch('https://swapi.dev/api/people/10/')
+  .then(response => response.json())
+  .then(data => {
+    const genderElement = document.getElementById('slide-kylo').querySelector('#gender');
+    genderElement.textContent = data.gender;
+
+    const cabeloElement = document.getElementById('slide-kylo').querySelector('#skin-color');
+    cabeloElement.textContent = data.hair_color;
+
+    const anoElement = document.getElementById('slide-kylo').querySelector('#ano-nascimento');
+    anoElement.textContent = data.birth_year;
+
+    const alturaElement = document.getElementById('slide-kylo').querySelector('#altura');
+    alturaElement.textContent = data.height;
+  });
+
+  fetch('https://swapi.dev/api/people/20/')
+  .then(response => response.json())
+  .then(data => {
+    const genderElement = document.getElementById('slide-yoda').querySelector('#gender');
+    genderElement.textContent = data.gender;
+
+    const cabeloElement = document.getElementById('slide-yoda').querySelector('#skin-color');
+    cabeloElement.textContent = data.hair_color;
+
+    const anoElement = document.getElementById('slide-yoda').querySelector('#ano-nascimento');
+    anoElement.textContent = data.birth_year;
+
+    const alturaElement = document.getElementById('slide-yoda').querySelector('#altura');
+    alturaElement.textContent = data.height;
+  });
+
+  fetch('https://swapi.dev/api/people/44/')
+  .then(response => response.json())
+  .then(data => {
+    const genderElement = document.getElementById('slide-maul').querySelector('#gender');
+    genderElement.textContent = data.gender;
+
+    const cabeloElement = document.getElementById('slide-maul').querySelector('#skin-color');
+    cabeloElement.textContent = data.hair_color;
+
+    const anoElement = document.getElementById('slide-maul').querySelector('#ano-nascimento');
+    anoElement.textContent = data.birth_year;
+
+    const alturaElement = document.getElementById('slide-maul').querySelector('#altura');
+    alturaElement.textContent = data.height;
+  });
